4e7a2244af9565048e4e03445d10ef87fd2c1efd
SuinaC <85263042+SuinaC@users.noreply.github.com>
2022-06-15 17:18:58 +0800

    Delete src/main/java/com/jia directory

--- src/main/java/com/jia/mapper/ArticleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.jia.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jia.domain.entity.Article;
-
-public interface ArticleMapper extends BaseMapper<Article> {
-}
--- src/main/java/com/jia/service/UploadService.java
+++
@@ -1,9 +0,0 @@
-package com.jia.service;
-
-import com.jia.domain.ResponseResult;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadService {
-    ResponseResult uploadImg(MultipartFile img);
-
-}
